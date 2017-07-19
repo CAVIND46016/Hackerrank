f839178d24b8fa167d5d3e7b9580852f79a408a9
Cavin Dsouza <cavin46016@gmail.com>
2017-07-19 01:24:50 -0400

    Optimized the code.

--- DrawingBook.py
+++ DrawingBook.py
@@ -2,18 +2,13 @@
 # https://www.hackerrank.com/contests/w27/challenges/drawing-book
 
 import sys
-
-def numOfPages_front(n, p):
-    return (p // 2);
-
-def numOfPages_back(n, p):
-    return (n - p) // 2;
-    
 def getMinNumOfPages(n, p):
-    if(numOfPages_front(n, p) < numOfPages_back(n, p)):
-        return numOfPages_front(n, p);
+    numOfPages_front = p // 2;
+    numOfPages_back = (n - p) // 2;
+    if(numOfPages_front < numOfPages_back):
+        return numOfPages_front;
     else:
-        return numOfPages_back(n, p);
+        return numOfPages_back;
     
 n = int(input().strip())
 p = int(input().strip())
